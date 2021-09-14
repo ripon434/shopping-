b6e060ca51b70db6fb1ea693f6941181b38df4a6
ripon434 <sdripon39@gmail.com>
2021-09-14 20:38:07 +0900

    first commit

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const loadProducts = () => {
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
@@ -34,8 +34,10 @@ const addToCart = (id, price) => {
   document.getElementById("total-Products").innerText = count;
 };
 
+
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
+  // console.log(element);
   const converted = parseInt(element);
   return converted;
 };
@@ -45,12 +47,12 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value.toFixed(2);
 };
 
 // update delivery charge and total Tax
@@ -68,13 +70,22 @@ const updateTaxAndCharge = () => {
     setInnerText("delivery-charge", 60);
     setInnerText("total-tax", priceConverted * 0.4);
   }
+ 
+  
 };
+
+loadProducts();
 
 //grandTotal update function
 const updateTotal = () => {
-  const grandTotal =
-    getInputValue("price") + getInputValue("delivery-charge") +
-    getInputValue("total-tax");
+  const grandTotal = 
+  getInputValue("price") + getInputValue("delivery-charge") +
+   getInputValue("total-tax");
+  console.log(grandTotal);
   document.getElementById("total").innerText = grandTotal;
-};
-loadProducts();
+}; 
+
+updateTotal();
+
+
+
